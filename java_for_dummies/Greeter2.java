06b544111670410605452d74ab839deb40cf7f6e
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-01-18 21:11:34 +0000

    added some program 180119

---
+++ java_for_dummies/Greeter2.java
@@ -0,0 +1,19 @@
+ // This class creates a Greeter object
+ // that displays a hello message
+ // in a dialog box.
+
+ import javax.swing.JOptionPane;
+
+ public class Greeter2
+ {
+ 	public void sayHello()
+ 	{
+ 		JOptionPane.showMessageDialog(null,"Hello, World!", "Greeter",
+ 		JOptionPane.INFORMATION_MESSAGE);
+
+
+ 	}
+
+ }
+
+
