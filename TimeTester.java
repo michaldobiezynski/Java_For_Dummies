1b78dca951b3c06c456df6516a950b57a8e5ddcb
Michal Dobiezynski <michaldobiezynski@hotmail.com>
2019-03-05 21:43:47 +0000

    a couple more

---
+++ TimeTester.java
@@ -0,0 +1,12 @@
+import java.util.*; import java.time.*;
+public class TimeTester {
+public static void main(String[] args) {
+System.out.println("\nLocalTime: " + LocalTime.now().toString());
+System.out.println("\nLocalDateTime: " + LocalDateTime.now().toString()); 
+System.out.println("\nZonedDateTime: " + ZonedDateTime.now().toString());
+System.out.println("\nOffsetTime: " + OffsetTime.now().toString());
+System.out.println("\nOffsetDateTime: " + OffsetDateTime.now().toString());
+System.out.println("\nMonthDay: " + MonthDay.now().toString());
+System.out.println("\nYearMonth: " + YearMonth.now().toString());
+System.out.println("\nInstant: " + Instant.now().toString());
+} }
